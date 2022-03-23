1e51d21f3513b05922c57473379a6c5b346c0a61
Desiree Pelletier <pelletier.desiree@gmail.com>
2022-03-22 21:10:55 -0400

    initial commit

---
+++ FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram{
+    public static void main(String[] args){
+        System.out.println("My name is Desiree");
+        System.out.println("I'm 33 years old");
+        System.out.println("I'm from Webster, MA");
+    }
+}
